db76cc23779af56034be4eb879d4449c478c1df8
shimanuki takayoshi <t-shima@db3.so-net.ne.jp>
2016-08-27 21:38:03 +0900

    Calc02スタート

---
+++ Calc2/Calc2/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Windows フォーム デザイナーで必要です。
+  Private components As System.ComponentModel.IContainer
+
+  'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+  'Windows フォーム デザイナーを使用して変更できます。  
+  'コード エディターを使って変更しないでください。
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
+    Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+    Me.FToolStripMenuItemFile = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemFileExit = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemSetting = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemSettingShowMenu = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemHelp = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemHelpReadme = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemHelpWeb = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripMenuItemHelpVersion = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripContainer1.TopToolStripPanel.SuspendLayout()
+    Me.ToolStripContainer1.SuspendLayout()
+    Me.MenuStrip1.SuspendLayout()
+    Me.SuspendLayout()
+    '
+    'ToolStripContainer1
+    '
+    Me.ToolStripContainer1.BottomToolStripPanelVisible = False
+    '
+    'ToolStripContainer1.ContentPanel
+    '
+    Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(284, 237)
+    Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.ToolStripContainer1.LeftToolStripPanelVisible = False
+    Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 0)
+    Me.ToolStripContainer1.Name = "ToolStripContainer1"
+    Me.ToolStripContainer1.RightToolStripPanelVisible = False
+    Me.ToolStripContainer1.Size = New System.Drawing.Size(284, 261)
+    Me.ToolStripContainer1.TabIndex = 0
+    Me.ToolStripContainer1.Text = "ToolStripContainer1"
+    '
+    'ToolStripContainer1.TopToolStripPanel
+    '
+    Me.ToolStripContainer1.TopToolStripPanel.Controls.Add(Me.MenuStrip1)
+    '
+    'MenuStrip1
+    '
+    Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.None
+    Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FToolStripMenuItemFile, Me.ToolStripMenuItemSetting, Me.ToolStripMenuItemHelp})
+    Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+    Me.MenuStrip1.Name = "MenuStrip1"
+    Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+    Me.MenuStrip1.TabIndex = 0
+    Me.MenuStrip1.Text = "MenuStrip1"
+    '
+    'FToolStripMenuItemFile
+    '
+    Me.FToolStripMenuItemFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItemFileExit})
+    Me.FToolStripMenuItemFile.Name = "FToolStripMenuItemFile"
+    Me.FToolStripMenuItemFile.Size = New System.Drawing.Size(66, 20)
+    Me.FToolStripMenuItemFile.Text = "ファイル(&F)"
+    '
+    'ToolStripMenuItemFileExit
+    '
+    Me.ToolStripMenuItemFileExit.Name = "ToolStripMenuItemFileExit"
+    Me.ToolStripMenuItemFileExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
+    Me.ToolStripMenuItemFileExit.Size = New System.Drawing.Size(155, 22)
+    Me.ToolStripMenuItemFileExit.Text = "終了(&X)"
+    '
+    'ToolStripMenuItemSetting
+    '
+    Me.ToolStripMenuItemSetting.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItemSettingShowMenu})
+    Me.ToolStripMenuItemSetting.Name = "ToolStripMenuItemSetting"
+    Me.ToolStripMenuItemSetting.Size = New System.Drawing.Size(57, 20)
+    Me.ToolStripMenuItemSetting.Text = "設定(&S)"
+    '
+    'ToolStripMenuItemSettingShowMenu
+    '
+    Me.ToolStripMenuItemSettingShowMenu.Name = "ToolStripMenuItemSettingShowMenu"
+    Me.ToolStripMenuItemSettingShowMenu.Size = New System.Drawing.Size(190, 22)
+    Me.ToolStripMenuItemSettingShowMenu.Text = "常にﾒﾆｭｰを表示する(&M)"
+    '
+    'ToolStripMenuItemHelp
+    '
+    Me.ToolStripMenuItemHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItemHelpReadme, Me.ToolStripMenuItemHelpWeb, Me.ToolStripMenuItemHelpVersion})
+    Me.ToolStripMenuItemHelp.Name = "ToolStripMenuItemHelp"
+    Me.ToolStripMenuItemHelp.Size = New System.Drawing.Size(65, 20)
+    Me.ToolStripMenuItemHelp.Text = "ヘルプ(&H)"
+    '
+    'ToolStripMenuItemHelpReadme
+    '
+    Me.ToolStripMenuItemHelpReadme.Name = "ToolStripMenuItemHelpReadme"
+    Me.ToolStripMenuItemHelpReadme.Size = New System.Drawing.Size(182, 22)
+    Me.ToolStripMenuItemHelpReadme.Text = "ReadMe.txtを表示(&R)"
+    '
+    'ToolStripMenuItemHelpWeb
+    '
+    Me.ToolStripMenuItemHelpWeb.Name = "ToolStripMenuItemHelpWeb"
+    Me.ToolStripMenuItemHelpWeb.Size = New System.Drawing.Size(182, 22)
+    Me.ToolStripMenuItemHelpWeb.Text = "Webサイトを表示(&W)"
+    '
+    'ToolStripMenuItemHelpVersion
+    '
+    Me.ToolStripMenuItemHelpVersion.Name = "ToolStripMenuItemHelpVersion"
+    Me.ToolStripMenuItemHelpVersion.Size = New System.Drawing.Size(182, 22)
+    Me.ToolStripMenuItemHelpVersion.Text = "バージョン情報(&V)"
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(284, 261)
+    Me.Controls.Add(Me.ToolStripContainer1)
+    Me.MainMenuStrip = Me.MenuStrip1
+    Me.Name = "Form1"
+    Me.Text = "Form1"
+    Me.ToolStripContainer1.TopToolStripPanel.ResumeLayout(False)
+    Me.ToolStripContainer1.TopToolStripPanel.PerformLayout()
+    Me.ToolStripContainer1.ResumeLayout(False)
+    Me.ToolStripContainer1.PerformLayout()
+    Me.MenuStrip1.ResumeLayout(False)
+    Me.MenuStrip1.PerformLayout()
+    Me.ResumeLayout(False)
+
+  End Sub
+
+  Friend WithEvents ToolStripContainer1 As ToolStripContainer
+  Friend WithEvents MenuStrip1 As MenuStrip
+  Friend WithEvents FToolStripMenuItemFile As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemFileExit As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemSetting As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemSettingShowMenu As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemHelp As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemHelpReadme As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemHelpWeb As ToolStripMenuItem
+  Friend WithEvents ToolStripMenuItemHelpVersion As ToolStripMenuItem
+End Class
---
+++ Calc2/Calc2/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+
+  Private Property ShowMenu As Boolean
+    Get
+      Return My.Settings.ShowMenu
+    End Get
+    Set(ByVal value As Boolean)
+      My.Settings.ShowMenu = value
+      ToolStripMenuItemSettingShowMenu.Checked = value
+      MenuStrip1.Visible = value
+    End Set
+  End Property
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Me.KeyPreview = True
+    ShowMenu = My.Settings.ShowMenu
+
+    Me.Text = Application.ProductName
+    Me.MinimumSize = New Drawing.Size(300, 100)
+    Me.Size = My.Settings.Size
+
+    Me.Location = My.Settings.Location
+    If Me.Left < Screen.GetWorkingArea(Me).Left OrElse
+       Me.Left >= Screen.GetWorkingArea(Me).Right Then
+      Me.Left = 100
+    End If
+    If Me.Top < Screen.GetWorkingArea(Me).Left OrElse
+       Me.Left >= Screen.GetWorkingArea(Me).Right Then
+      Me.Left = 100
+    End If
+  End Sub
+
+  Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+    If e.Alt Then MenuStrip1.Visible = True
+  End Sub
+
+  Private Sub ToolStripMenuItemFileExit_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItemFileExit.Click
+    Application.Exit()
+  End Sub
+
+  Private Sub MenuStrip1_MenuDeactivate(sender As Object, e As EventArgs) Handles MenuStrip1.MenuDeactivate
+    If Not ShowMenu Then MenuStrip1.Visible = False
+  End Sub
+
+  Private Sub ToolStripMenuItemSettingShowMenu_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItemSettingShowMenu.Click
+    ShowMenu = Not ShowMenu
+  End Sub
+
+  Private Sub ToolStripMenuItemHelpReadme_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItemHelpReadme.Click
+    Dim s = IO.Path.GetDirectoryName(Application.ExecutablePath)
+    s = IO.Path.Combine(s, "readme.txt")
+    If IO.File.Exists(s) Then
+      Process.Start(s)
+    Else
+      MessageBox.Show(s & "が見つかりません", "エラー")
+    End If
+  End Sub
+
+  Private Sub ToolStripMenuItemHelpWeb_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItemHelpWeb.Click
+    Process.Start("http://www.yahoo.co.jp")
+  End Sub
+
+  Private Sub ToolStripMenuItemHelpVersion_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItemHelpVersion.Click
+    Dim s = ""
+    s = Application.ProductName & " " & Application.ProductVersion & " (2016/02/22)" + Environment.NewLine
+    Dim fileVersionInfo = Diagnostics.FileVersionInfo.GetVersionInfo(Application.ExecutablePath)
+    Dim copyright = fileVersionInfo.LegalCopyright
+    s &= copyright & Environment.NewLine & Environment.NewLine
+    s &= "実行ファイル：" & Environment.NewLine & Application.ExecutablePath & Environment.NewLine & "("
+    If Environment.Is64BitProcess Then
+      s &= "64"
+    Else
+      s &= "32"
+    End If
+    s &= "ビット・プロセスとして稼働）" & Environment.NewLine & Environment.NewLine
+    Dim c = New Devices.Computer
+    s &= "オペレーティングシステム：" &
+      Environment.NewLine & c.Info.OSFullName & " " & c.Info.OSVersion & " "
+    If Environment.Is64BitOperatingSystem Then
+      s &= "64"
+    Else
+      s &= "32"
+    End If
+    s &= "ビット"
+    MessageBox.Show(s, "バージョン情報")
+  End Sub
+
+  Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+    Me.WindowState = FormWindowState.Normal
+    My.Settings.Location = Me.Location
+    My.Settings.Size = Me.Size
+  End Sub
+End Class
